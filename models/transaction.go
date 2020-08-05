168555f985757e577fd62da526ba0f81d6cbd5ea
dothiphuc81299 <48743415+dothiphuc81299@users.noreply.github.com>
2020-08-05 16:42:40 +0700

    Update transaction.go

--- models/transaction.go
+++ models/transaction.go
@@ -7,6 +7,7 @@ import (
 )
 
 type (
+
 	TransactionBSON struct {
 		ID             primitive.ObjectID `bson:"_id"`
 		CompanyID      primitive.ObjectID `bson:"companyId"`
@@ -16,5 +17,24 @@ type (
 		Commission     float64            `bson:"commission"`
 		LoyaltyProgram float64            `bson:"loyaltyprogram"`
 		CreateAt       time.Time          `bson:"createAt"`
+	}
+	//TransactionDetail ...
+	TransactionDetail struct {
+		ID             primitive.ObjectID `json:"_id"`
+		CompanyID      primitive.ObjectID `json:"companyId"`
+		BranchID       primitive.ObjectID `json:"branchId"`
+		User           string             `json:"user"`
+		Amount         float64            `json:"amount"`
+		Commission     float64            `json:"commission"`
+		LoyaltyProgram float64            `json:"loyaltyprogram"`
+		CreateAt       time.Time          `json:"createAt"`
+	}
+	//PostTransaction is a  struct of body request
+	PostTransaction struct {
+		NameCompany    string  `json:"nameCompany" valid:"stringlength(3|30),type(string)"`
+		NameBranch     string  `json:"nameBranch" valid:"stringlength(3|30),type(string)"`
+		User           string  `json:"user" valid:"stringlength(3|30),type(string)"`
+		Amount         float64 `json:"amount" valid:"required"`
+		Commission float64     `json:"commission"`
 	}
 )
