7ebc9b3f847dfc8bf1e922343f9a954dac66ae70
dothiphuc81299 <48743415+dothiphuc81299@users.noreply.github.com>
2020-08-10 16:18:50 +0700

    Update transaction.go

--- dao/transaction.go
+++ dao/transaction.go
@@ -3,16 +3,11 @@ package dao
 import (
 	"context"
 	"time"
-<<<<<<< HEAD
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"cashbag-me-mini/models"
 	"cashbag-me-mini/modules/database"
-=======
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
->>>>>>> 35bc62c3407eecf25d9e630ee81dc51e89f7e4bb
 )
 
 // TransactionCreate ....
@@ -22,7 +17,6 @@ func TransactionCreate(doc models.TransactionBSON, balance float64) (models.Tran
 		ctx        = context.Background()
 	)
 
-<<<<<<< HEAD
 	// Add update information
 	if doc.ID.IsZero() {
 		doc.ID = primitive.NewObjectID()
@@ -37,18 +31,6 @@ func TransactionCreate(doc models.TransactionBSON, balance float64) (models.Tran
 		CompanyUpdateBalance(doc.CompanyID, balanceCurrent)
 		TransactionAnalyticHandle(doc)
 	}
-=======
-	//Add update information
-	if doc.ID.IsZero() {
-		doc.ID = primitive.NewObjectID()
-	}
-	doc.CreatedAt = time.Now()
-	balanceCurrent := balance - doc.Commission
-	_, err := collection.InsertOne(ctx, doc)
-
-	UpdateBalance(doc.ID, balanceCurrent)
-	HandleTranAnalytic(doc)
->>>>>>> 35bc62c3407eecf25d9e630ee81dc51e89f7e4bb
 
 	return doc, err
 }
