56a939a60d67d9a196842c12100c881b8d9fa38c
dothiphuc81299 <48743415+dothiphuc81299@users.noreply.github.com>
2020-08-10 16:12:28 +0700

    Update transaction.go

--- models/transaction.go
+++ models/transaction.go
@@ -16,11 +16,7 @@ type (
 		Amount         float64            `bson:"amount"`
 		Commission     float64            `bson:"commission"`
 		LoyaltyProgram float64            `bson:"loyaltyprogram"`
-<<<<<<< HEAD
 		CreatedAt      time.Time          `bson:"createdAt"`
-=======
-		CreatedAt       time.Time          `bson:"createdAt"`
->>>>>>> 35bc62c3407eecf25d9e630ee81dc51e89f7e4bb
 	}
 
 	//TransactionDetail ...
@@ -32,7 +28,6 @@ type (
 		Amount         float64            `json:"amount"`
 		Commission     float64            `json:"commission"`
 		LoyaltyProgram float64            `json:"loyaltyprogram"`
-<<<<<<< HEAD
 		CreatedAt      time.Time          `json:"createdAt"`
 	}
 
@@ -42,15 +37,5 @@ type (
 		BranchID  string  `json:"branchID"`
 		User      string  `json:"user" valid:"stringlength(3|100),type(string)"`
 		Amount    float64 `json:"amount"`
-=======
-		CreatedAt       time.Time          `json:"createdAt"`
-	}
-	// TransactionCreatePayload is a  struct of body request
-	TransactionCreatePayload struct {
-		CompanyID      primitive.ObjectID `json:"companyId"`
-		BranchID       primitive.ObjectID `json:"branchId"`
-		User           string  `json:"user" valid:"stringlength(3|30),type(string)"`
-		Amount         float64 `json:"amount"`
->>>>>>> 35bc62c3407eecf25d9e630ee81dc51e89f7e4bb
 	}
 )
