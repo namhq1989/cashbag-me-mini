7cd3e921dd9d3e6aae5895d5fe2c530c90c2c34b
Hoang <lequanghoang99@outlook.com>
2020-08-17 06:49:19 +0700

    Add Handle postpaidLog

--- services/company_analytic.go
+++ services/company_analytic.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"errors"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"cashbag-me-mini/dao"
 	"cashbag-me-mini/models"
@@ -27,12 +30,31 @@ func transactionAnalyticHandleForTransaction(transaction models.TransactionBSON)
 	if transaction.Postpaid == true {
 		companyAnalytic.TotalDebt += transaction.Commission
 		companyAnalytic.CountPostpaid++
+		countPostpaid := companyAnalytic.CountPostpaid
+		err = postpaidLogHandle(countPostpaid, companyID)
+		if err != nil {
+			err = errors.New("postpaidLogHandle That Bai")
+			return
+		}
 	}
 
 	// Update CompanyAnalytic
 	err = dao.CompanyAnalyticUpdateTransactionProperties(companyAnalytic)
 	if err != nil {
 		err = errors.New("Update CompanyAnalytic That Bai")
+		return
+	}
+	return
+}
+
+// postpaidLogHandle
+func postpaidLogHandle(countPostpaid int, companyID primitive.ObjectID) (err error) {
+	if countPostpaid > 3 {
+		log.Println("Số đơn hàng trả sau vượt mức cho phép")
+	}
+	if countPostpaid > 6 {
+		log.Println("Số đơn hàng trả sau vượt mức cho phép và deactive company")
+		_, err = dao.CompanyChangeActiveStatus(companyID)
 		return
 	}
 	return
