2f1566e73d04887bba96336401c531928090a22c
dothiphuc81299 <48743415+dothiphuc81299@users.noreply.github.com>
2020-08-18 21:59:59 +0700

    Update company.go

--- dao/company.go
+++ dao/company.go
@@ -104,12 +104,29 @@ func CompanyFindByID(id primitive.ObjectID) (models.CompanyBSON, error) {
 	return result, err
 }
 
+
 // CompanyUpdateBalance ...
 func CompanyUpdateBalance(id primitive.ObjectID, balance float64) {
 	var (
 		filter = bson.M{"_id": id}
 		update = bson.M{"$set": bson.M{
 			"balance": balance,
+		}}
+	)
+
+	// Update
+	err := CompanyUpdateByID(filter, update)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+// CompanyUpdateActive ...
+func CompanyUpdateActive(id primitive.ObjectID, active bool) {
+	var (
+		filter = bson.M{"_id": id}
+		update = bson.M{"$set": bson.M{
+			"active":active,
 		}}
 	)
 
@@ -133,3 +150,5 @@ func CompanyUpdateByID(filter bson.M, updateData bson.M) error {
 
 	return err
 }
+
+
